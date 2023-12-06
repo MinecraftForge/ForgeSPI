3a5479617365422f846074aa5740b881308c319e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-12-06 18:52:12 +0000

    Add `IModFileInfo#isClientSideOnly` (#19)

--- src/main/java/net/minecraftforge/forgespi/language/IModFileInfo.java
+++ src/main/java/net/minecraftforge/forgespi/language/IModFileInfo.java
@@ -19,6 +19,14 @@ public interface IModFileInfo {
 
     boolean showAsResourcePack();
 
+    /**
+     * Indicates if this mod file is only intended for physical clients.
+     * <p>When true, the loader will skip loading this mod file on dedicated servers.</p>
+     */
+    default boolean isClientSideOnly() {
+        return false;
+    }
+
     Map<String,Object> getFileProperties();
 
     String getLicense();
